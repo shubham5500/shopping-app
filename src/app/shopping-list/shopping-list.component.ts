8ce1ac5af73b3cdab520162674fd05906b5daa5b
badmofo <shubham.sy570@gmail.com>
2020-01-27 18:24:27 +0530

    initial commit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {List, ListItem} from './list';
+import {StorageService} from '../../shared/storage.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,24 +15,29 @@ export class ShoppingListComponent implements OnInit {
   constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.lists = StorageService.getItem('lists');
   }
 
   addList() {
     this.lists.push(new List('', [new ListItem('', 0)]));
+    StorageService.setItem('lists', this.lists);
   }
 
   addListItemOnEnter(event: any, listIndex) {
     if (event.keyCode === 13) {
       this.lists[listIndex].items.push(new ListItem('', 0));
+      StorageService.setItem('lists', this.lists);
     }
   }
 
   addListItem(listIndex, itemIndex) {
     this.lists[listIndex].items.push(new ListItem('', 0));
+    StorageService.setItem('lists', this.lists);
   }
 
   removeListItem(listIndex, itemIndex) {
     this.lists[listIndex].items.splice(itemIndex, 1);
+    StorageService.setItem('lists', this.lists);
   }
 
   addOrRemoveListItemQuantity(quantity, listIndex, itemIndex) {
@@ -39,9 +45,11 @@ export class ShoppingListComponent implements OnInit {
       return;
     }
     this.lists[listIndex].items[itemIndex].quantity = this.lists[listIndex].items[itemIndex].quantity + parseInt(quantity, 10);
+    StorageService.setItem('lists', this.lists);
   }
 
   removeList(listIndex) {
     this.lists.splice(listIndex, 1);
+    StorageService.setItem('lists', this.lists);
   }
 }
